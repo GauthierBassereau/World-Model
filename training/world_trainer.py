d362c1d5466d8508077c71d5680acf0f5369d205
Ubuntu <gauthier.bassereau@gmail.com>
2025-10-19 09:42:04 +0000

    config stuff

--- training/world_trainer.py
+++ training/world_trainer.py
@@ -54,8 +54,8 @@ class LoggingConfig:
     checkpoint_interval: int = 1_000
     output_dir: str = "checkpoints"
     sample_interval: Optional[int] = 1_000
-    log_tau_histograms: bool = False
-    tau_log_limit: int = 0
+    log_tau_histograms: bool = True
+    tau_log_limit: int = 200
 
 
 @dataclass
